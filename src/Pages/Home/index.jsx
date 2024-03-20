2fd90158fb07df1c2a45e2e8e8079431b957a0a3
Sohail-Yawer <sohailyawer9@gmail.com>
2024-03-20 17:15:32 -0500

    Added sidebar shopping cart

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
 import Navbar from '../../components/NavBar/navbar.component';
 
 
 
 const Home = () => {
+  const [sidebarToggled,setSidebarToggled] = useState(false);
+  const sidebarRef = useRef(null);
+  useEffect(() => {
+    function handler(e){
+      if(sidebarRef.current){
+        if(!e.target.classList.contains("sidebar") && !e.target.classList.contains("sidebar-toggle")){
+          setSidebarToggled(false);
+        }
+      }
+    }
+    document.addEventListener("click",handler);
+    return () => {
+      document.removeEventListener("click",handler);
+    }
+  },[]);
+  const toggleSidebar = () => {
+    setSidebarToggled(!sidebarToggled);
+  };
   return (
     <div> 
-      <Navbar></Navbar>
+            <Navbar toggleSidebar={toggleSidebar} />
+
+     
+      <aside ref={sidebarRef} className={`sidebar ${sidebarToggled ? "visible" :"" }`}>Sidebar</aside>
       Home
     </div>
     
--- src/components/NavBar/navbar.component.jsx
+++ src/components/NavBar/navbar.component.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
-import { Button, Container, Nav, Navbar as NavbarBs } from 'react-bootstrap';
+import { Button,Container, Nav, Navbar as NavbarBs } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import "./navbar.style.css";
+import { FiShoppingCart } from "react-icons/fi";
+import "../../App.css";
 
-const Navbar = () => {
+
+const Navbar = ({ toggleSidebar }) => {
   return (
     <div>
       <NavbarBs sticky="top" className='bg-white shadow-sm mb-3'>
-        <Container >
+        <Container>
           <Nav className='container-fluid'>
             <Nav.Link className='custom-button ms-auto' as={NavLink} to="/" exact>
               Home
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/About" className='custom-button '>
+            <Nav.Link as={NavLink} to="/About" className='custom-button'>
               About
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/Contact" className='custom-button '>
+            <Nav.Link as={NavLink} to="/Contact" className='custom-button'>
               Contact
             </Nav.Link>
-            
-            <Button className='custom-button'
-            style={{ width: "3rem", height: "3rem" }}
-            variant='outline'
-          >
-            <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="16.5" cy="18.5" r="1.5" />
-              <circle cx="9.5" cy="18.5" r="1.5" />
-              <path d="M18 16H8a1 1 0 0 1-.958-.713L4.256 6H3a1 1 0 0 1 0-2h2a1 1 0 0 1 .958.713L6.344 6H21a1 1 0 0 1 .937 1.352l-3 8A1 1 0 0 1 18 16zm-9.256-2h8.563l2.25-6H6.944z" />
-            </svg>
-          </Button>
-         
+            <div className="sidebar-toggle" onClick={toggleSidebar}>
+              {/* Your toggle button content */}
+              <Button
+                className='custom-button sidebar-toggle'
+                style={{ width: "3rem", height: "3rem", padding: "0.1rem" }}
+                variant='outline'
+              >
+                <FiShoppingCart className='sidebar-toggle' onClick={toggleSidebar}/>
+              </Button>
+            </div>
           </Nav>
-          
         </Container>
       </NavbarBs>
     </div>
